4aff6e76857adb140ac9f47d446220859b71e33b
Nandini Nayak <nandini.r.nayak@gmail.com>
2020-02-28 14:22:11 +1100

    foreign key changed for opal card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,5 @@
 class Card < ApplicationRecord
   belongs_to :profile
   self.primary_key = 'opal_number'
-  has_many :attendances, primary_key: 'opal_number', foreign_key: 'card_opal_number'
+  has_many :attendances, primary_key: 'opal_number', foreign_key: 'card_opal_number', dependent: :destroy
 end
